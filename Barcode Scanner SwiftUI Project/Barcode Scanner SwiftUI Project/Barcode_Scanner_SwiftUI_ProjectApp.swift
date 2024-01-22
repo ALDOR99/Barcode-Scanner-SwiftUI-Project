513aa66fdee62d9892bba564adaef802c84edce1
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2024-01-22 19:30:48 +0300

    .

--- Barcode Scanner SwiftUI Project/Barcode Scanner SwiftUI Project/Barcode_Scanner_SwiftUI_ProjectApp.swift
+++ Barcode Scanner SwiftUI Project/Barcode Scanner SwiftUI Project/Barcode_Scanner_SwiftUI_ProjectApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Barcode_Scanner_SwiftUI_ProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BarcodeScannerView()
         }
     }
 }
