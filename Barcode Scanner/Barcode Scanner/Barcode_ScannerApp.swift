efe73214bdd3d48682769453bf762fdf65142e81
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2024-01-25 14:16:44 +0300

    0.0

---
+++ Barcode Scanner/Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Ali Durna on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
